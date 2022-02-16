81787351973da347f6fcdbae4b754b6ef4928808
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-16 01:18:46 -0600

    Created and styled the navigation bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = () => {
+    return (
+        <div class="nav-wrapper">
+            <div class="nav-container">
+                <p>Linkonect</p>
+                <div class="link-container">
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>FAQ</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
