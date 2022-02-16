ef45f3b7c5c0942a7481095430ba480bc497b7ac
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-16 04:44:44 -0600

    Working on the hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+    return (
+        <div class="hero-wrapper">
+            <div class="hero-container">
+                <div class="first-section">
+                    <h1>Take the awkwardness out of asking for recommendations</h1>
+                    <img src="../images/recommend.png" alt="Recommendation from Linkedin"/>
+                </div>
+                <div class="second-section">
+                    <p>Second Section</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
